5074086baed4b20928290558801dad8e04ffa001
mxm2005 <mxmmm18@163.com>
2016-12-01 14:30:55 +0800

    mod

--- CodeCollect/Program.cs
+++ CodeCollect/Program.cs
@@ -10,7 +10,7 @@ namespace CodeCollect
         static void Main(string[] args)
         {
         	
-		//demo
+		//demo 测试文件编码格式判断
             string ff = "E:\\CheckResultPath\\DetailFile\\20161130\\18833-80887119085622.xml";
             //FileInfo inf = new FileInfo(ff);
             Console.WriteLine(JudgeFileEncodingType.GetType(ff).ToString());
